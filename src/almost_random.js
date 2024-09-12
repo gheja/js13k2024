bf651428e1325d4594d5f3c5603ee2cca0ea991f
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2024-09-12 21:25:01 +0200

    Added getRandomIndexWeighted()

--- src/almost_random.js
+++ src/almost_random.js
@@ -18,3 +18,28 @@ function getRandomPick(arr)
 {
     return arr[getRandomInt(0, arr.length)]
 }
+
+// arr is an array with the weights like [ 10, 10, 5, 5, 0, 0, 10 ]
+// returns the index that was picked by the given chances/weights
+// at least one item must be non-zero
+function getRandomIndexWeighted(arr)
+{
+	var total = 0
+
+	for (var a of arr)
+	{
+		total += a
+	}
+
+	var n = getRandomInt(0, total)
+
+	for (var i = 0; i < arr.length; i++)
+	{
+		n -= arr[i]
+
+		if (n < 0)
+		{
+			return i
+		}
+	}
+}
